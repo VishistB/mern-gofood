9061573b0760e134e5342e46c175da2a6e8f3ee2
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-06-20 02:54:07 +0530

    price calculation with one problem

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import './App.css';
-import Home from './screens/Home';
-import Login from './screens/Login';
-import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
-import Signup from './screens/Signup';
+import "./App.css";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./screens/Signup";
+import { CartProvider } from "./components/ContextReducer";
 // import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 // import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 // import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 function App() {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route exact path='/' element={<Home/>}/>
-          <Route exact path='/login' element={<Login/>}/>
-          <Route exact path='/signup' element={<Signup/>}/>
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <CartProvider>
+            <Router>
+                <div>
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        <Route exact path="/login" element={<Login />} />
+                        <Route exact path="/signup" element={<Signup />} />
+                    </Routes>
+                </div>
+            </Router>
+        </CartProvider>
+    );
 }
 
 export default App;
--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,12 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useCart, useDispatchCart } from "./ContextReducer";
 
-function Card({foodName, options,imgsrc}) {
-    const[modifiedFoodName,setmodifiedFoodName]=useState("");
-    let fudoptions = options;
+function Card(props) {
+    // const[modifiedFoodName,setmodifiedFoodName]=useState("");
+    let data=useCart()
+    let dispatch = useDispatchCart();
+    let fudoptions = props.options;
+    const priceRef=useRef();
     const priceOptions = Object.keys(fudoptions).filter((key) => key !== "_id");
-    const handleAddToCart = () => {
-
+    const[qty,setQty]=useState(1);
+    const[size,setSize]=useState("");
+    const handleAddToCart = async() => {
+        await dispatch({type:"ADD", id:props.foodItem._id, name:props.foodItem.name, price:finalPrice, qty:qty, size:size})
+        console.log(data)
     }
+    let finalPrice=qty*parseInt(fudoptions[size]);
+    useEffect(()=>{
+        setSize(priceRef.current.value)
+    },[])
     return (
         <div>
             <div
@@ -14,12 +25,12 @@ function Card({foodName, options,imgsrc}) {
                 style={{ width: "18rem", maxHeight: "360px" }}
             >   
                 {/* <div className="container w-100 h-40 p-0 overflow-hidden"> */}
-                    <img className="card-img-top" src={imgsrc} alt="Card image cap" style={{height:"150px",objectFit:"fill"}} />
+                    <img className="card-img-top" src={props.foodItem.img} alt="Card image cap" style={{height:"150px",objectFit:"fill"}} />
                 {/* </div> */}
                 <div className="card-body">
-                    <h5 className="card-title">{foodName}</h5>
+                    <h5 className="card-title">{props.foodItem.name}</h5>
                     {/* <p className="card-text">Some import Text here</p> */}
-                    <div className="container w-100">
+                    <div className="container w-100" onChange={(e)=>setQty(e.target.value)}>
                         <select className="m-2 h-100 bg-sucess rounded">
                             {Array.from(Array(6), (e, i) => {
                                 return (
@@ -30,14 +41,14 @@ function Card({foodName, options,imgsrc}) {
                             })}
                         </select>
 
-                        <select className="m-2 h-100 bg-sucess rounded">
+                        <select className="m-2 h-100 bg-sucess rounded" ref={priceRef} onChange={(e)=>setSize(e.target.value)}>
                             {priceOptions.map((data)=>{
                                 return(
                                     <option key={data} value={data}>{data}</option>
                                 )
                             })}
                         </select>
-                        <div className="d-inline h-100 fs-5">Total Price</div>
+                        <div className="d-inline h-100 fs-5">₹{finalPrice}/-</div>
                     </div>
                     <hr/>
                     <button className="btn btn-success justify-center ms-2" onClick={handleAddToCart}>Add to Card</button>
